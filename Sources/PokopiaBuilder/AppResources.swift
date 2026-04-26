b10c1fe95c36244bd88ab347a98f289d22f0c5d6
Kevin Howe <appleforever1994@gmail.com>
2026-04-26 14:45:09 -0400

    Fix packaged resource lookup

--- Sources/PokopiaBuilder/AppResources.swift
+++ Sources/PokopiaBuilder/AppResources.swift
@@ -2,10 +2,28 @@ import Foundation
 
 enum AppResources {
     static var bundle: Bundle {
-        #if SWIFT_PACKAGE
-        Bundle.module
-        #else
-        Bundle.main
-        #endif
+        resourceBundle ?? Bundle.main
     }
+
+    private static let resourceBundle: Bundle? = {
+        let bundleName = "PokopiaBuilder_PokopiaBuilder.bundle"
+        let fileManager = FileManager.default
+        var candidates: [URL] = []
+
+        if let resourceURL = Bundle.main.resourceURL {
+            candidates.append(resourceURL.appendingPathComponent(bundleName, isDirectory: true))
+        }
+
+        candidates.append(Bundle.main.bundleURL.appendingPathComponent(bundleName, isDirectory: true))
+        candidates.append(Bundle.main.bundleURL.appendingPathComponent("Contents/Resources/\(bundleName)", isDirectory: true))
+
+        if let executableURL = Bundle.main.executableURL {
+            candidates.append(executableURL.deletingLastPathComponent().appendingPathComponent(bundleName, isDirectory: true))
+        }
+
+        return candidates.lazy.compactMap { url in
+            guard fileManager.fileExists(atPath: url.path) else { return nil }
+            return Bundle(url: url)
+        }.first
+    }()
 }
